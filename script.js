2b7664613f9a2e5a31aa72e57d7a18d8c5030fdb
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-08-22 08:39:17 +1000

    Add remove button to book card

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ submitBtn.addEventListener("click", function (event) {
     userRead.checked
   );
   createBookCard();
-  console.log(myLibrary);
 });
 
 let myLibrary = [];
 
 function Book(title, author, pages, havRead) {
-  this.title = title;
+  this.title = `"${title}"`;
   this.author = author;
-  this.pages = pages;
+  this.pages = `${pages} pages`;
   this.havRead = havRead;
 }
 
 function addBookToLibrary(title, author, pages, havRead) {
+  havRead = havRead === true ? "Read" : "Not read";
   const newBook = new Book(title, author, pages, havRead);
   myLibrary.push(newBook);
 }
@@ -34,10 +34,19 @@ function addBookToLibrary(title, author, pages, havRead) {
 // Create a book card
 function createBookCard() {
   bookCardContainer.innerHTML = "";
+  populateBookCard();
+}
 
+function populateBookCard() {
   myLibrary.forEach(function (number) {
     let bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
+    bookCardContainer.appendChild(bookCard);
+    let removeButton = document.createElement("button");
+    removeButton.classList.add("remove-button");
+    bookCard.prepend(removeButton);
+    removeButton.textContent = "Remove";
+
     let text = "";
     for (let x in number) {
       text += number[x];
@@ -46,6 +55,5 @@ function createBookCard() {
       bookCard.appendChild(infoField);
       infoField.textContent = number[x];
     }
-    bookCardContainer.appendChild(bookCard);
   });
 }
